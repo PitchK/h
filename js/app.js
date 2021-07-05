14f8f505aabcd6d386b8a49763f9c8b17a2540c9
unknown <capem22@maryknollschool.org>
2021-07-04 19:32:32 -1000

    added images

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*for (var i = 0; i < document.getElementsByClassName('images').length; i++){
+    document.getElementsByClassName('images')[i].addEventListener('click', enlarge);
+}
+
+function enlarge(){
+    for (var i = 0; i < document.getElementsByClassName('images').length; i++){
+        document.getElementsByClassName('images')[i].style.transform = 'scale(1)';
+    }
+    this.style.transform = 'scale(3)';
+}*/

ed04cb79bbf5f2559cdbcc659df4b4747b9f138c
unknown <capem22@maryknollschool.org>
2021-07-07 00:00:52 -1000

    mobile compatible

--- js/app.js
+++ js/app.js
@@ -8,3 +8,15 @@ function enlarge(){
     }
     this.style.transform = 'scale(3)';
 }*/
+
+function show(){
+    if(document.getElementById('mobileHeaderOpen').style.display === 'block'){
+        document.getElementById('mobileHeaderOpen').style.display = 'none';
+        document.getElementById('mobileHeader').style.borderBottom = '2px solid black';
+    }else{
+        document.getElementById('mobileHeaderOpen').style.display = 'block';
+        document.getElementById('mobileHeader').style.borderBottom = 'none';
+    }
+}
+
+document.getElementById('menu').addEventListener('click', show);
